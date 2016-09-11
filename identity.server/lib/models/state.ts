c7abd81d7d081aac4f836dbedb9d9c67a7fe5ebc
Ajay Singh <meajaysingh@hotmail.com>
2016-09-11 06:28:48 +0530

    fix: ref type

--- identity.server/lib/models/state.ts
+++ identity.server/lib/models/state.ts
@@ -4,7 +4,7 @@ import { model, Schema, SchemaType } from 'mongoose';
 const StateSchemaObject = {
     name: String,
     abbr: String,
-    country: { type: Number, ref: 'Plzoo_Countries' }
+    country: { type: Schema.Types.ObjectId, ref: 'Plzoo_Countries' }
 };
 
 export const StateSchema = new Schema('Plzoo_States',StateSchemaObject);
