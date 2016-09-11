c8a9440661492038b686f70a03f57b25997da8e8
Ajay Singh <meajaysingh@hotmail.com>
2016-09-11 06:19:53 +0530

    unittest: user mgt

---
+++ identity.server/tests/index.ts
@@ -0,0 +1,9 @@
+/* @license
+ * Copyright pLzOO Inc. All Rights Reserved.
+ * Project: identity.server
+ * File: tests/index.ts
+ * Author: Ajay Singh <meajaysingh@hotmail.com> 
+ */
+
+import '../server';
+import './modules';
